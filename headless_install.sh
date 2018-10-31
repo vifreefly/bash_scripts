82544d9ebd6ca4e97d221a5b156deece53d1670a
Victor Afanasev <vicfreefly@gmail.com>
2018-10-31 18:35:45 +0400

    Change drivers versions

--- headless_install.sh
+++ headless_install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # check latest here https://sites.google.com/a/chromium.org/chromedriver/downloads
-# note: for now there is harcoded 2.39 version, 2.40 version is too baggy
-CHROMEDRIVER_VERSION=2.39
+CHROMEDRIVER_VERSION=2.43
 
 # check latest here https://github.com/mozilla/geckodriver/releases/
-GECKODRIVER_VERSION=0.21.0
+GECKODRIVER_VERSION=0.23.0
 
 # check latest here http://phantomjs.org/download.html
 PHANTOMJS_VERSION=2.1.1
